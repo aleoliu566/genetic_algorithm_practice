cdd8bb50185d3baecf1f3e65c2ac8f6755c068e1
leo <aleoliu566@gmail.com>
2019-01-20 20:14:53 +0800

    save best genetic

--- ga_scheduling.py
+++ ga_scheduling.py
@@ -2,17 +2,19 @@ from statistics import *
 from numpy import *
 from random import *
 from operator import itemgetter
+from copy import deepcopy
 
-# 0 日班
-# 1 夜班
-# 2 大夜班
+# 0 日班 8人
+# 1 夜班 6人
+# 2 大夜班 4人
 # 3 放假
 
 CROSSOVER_RATE = 0.8
 MUTATION_RATE = 0.1
-ITERATION_TIME = 20     #迭代次數
+ITERATION_TIME = 1000     #迭代次數
 NUMBER_OF_GENETIC = 10   #基因數量
-NUMBER_OF_WORKER = 18    #工作人數
+NUMBER_OF_WORKER = 35    #工作人數
+WORK_DAY = 28
 
 
 # 染色體集合
@@ -20,13 +22,43 @@ all_genetic = []
 best_genetic = []
 best_genetic_target_value = 1000000000000
 
+def dayShiftScheduleScore(genetic, needToReachNumber, workType, showValue=False):
+  score = 10
+  shift_numbers = [0]*28
+
+  # 記錄每天日班有多少人排班
+  for i in range(len(genetic)):
+    for j in range(WORK_DAY):
+      if(genetic[i][j] == workType):
+          shift_numbers[j] += 1
+
+  day_did_not_reach_number = sum(i < needToReachNumber for i in shift_numbers)
+
+  if showValue:
+    print('沒有達到',needToReachNumber,'人的天數', day_did_not_reach_number)
+
+  score = score * day_did_not_reach_number
+  return score
+
+def three_work_type_score(teamSchedule,showValue):
+  # showValue = True
+  # 判斷是否每天日班有8人上班，回傳一個分數
+  dayShiftScore = dayShiftScheduleScore(teamSchedule, 8, 0, showValue)
+  # 判斷是否每夜班有6人上班，回傳一個分數
+  laterShiftScore = dayShiftScheduleScore(teamSchedule, 6, 1, showValue)
+  # 判斷是否每天大夜班有4人上班，回傳一個分數
+  graveShiftScore = dayShiftScheduleScore(teamSchedule, 4, 2, showValue)
+
+  score = dayShiftScore + laterShiftScore + graveShiftScore
+  return score
+
 def targetFunction(teamSchedule, showValue=False):
-  # O -> 休假次數
   # µ0 -> 平均休假次數
   # 變異數
   day_shift = [] #日班
   later_shift = []
   grave_shift = []
+  day_off = []
   for i in range(NUMBER_OF_WORKER):
     # 日班
     day_shift.append(len([i for i in teamSchedule[i] if i == 0]))
@@ -34,8 +66,12 @@ def targetFunction(teamSchedule, showValue=False):
     later_shift.append(len([i for i in teamSchedule[i] if i == 1]))
     # 大夜班
     grave_shift.append(len([i for i in teamSchedule[i] if i == 2]))
+    # 休假次數
+    day_off.append(len([i for i in teamSchedule[i] if i == 3]))
 
-  target_value = round(variance(day_shift)+variance(later_shift)+variance(grave_shift), 10)
+  ShiftNumberBasicScore = three_work_type_score(teamSchedule,showValue)
+
+  target_value = round(variance(day_shift)+variance(later_shift)+variance(grave_shift)+variance(day_off), 10) + ShiftNumberBasicScore
   if showValue:
     print('Total variance: ', target_value )
   return target_value
@@ -80,7 +116,7 @@ def muation():
 
   new_genetic = generateEachWorker()[:]
   all_genetic[genetic_mutation][worker_mutation] = new_genetic[:]
-  print('突變')
+  # print('突變')
   return
 
 def generateEachWorker():
@@ -122,6 +158,7 @@ def initializeGenetic():
 def selectNextGeneration(number_of_generation):
   global all_genetic
   global best_genetic_target_value
+  global best_genetic
   temp_all_genetic = []
   biggestTargetValue = -1
   population = []
@@ -150,15 +187,18 @@ def selectNextGeneration(number_of_generation):
     temp_all_genetic.append(all_genetic[x[0]][:] )
   all_genetic = temp_all_genetic[:]
 
-  print('\n','============= ITERATION ', number_of_generation ,' =============')
+  print('\n','======================= ITERATION ', number_of_generation ,' =======================')
   for i in range(NUMBER_OF_GENETIC): # 印出genetic 變異數
     targetValue = targetFunction(all_genetic[i], True)
 
     # 記錄最好的基因
-    if(best_genetic_target_value > targetFunction(all_genetic[i]) ):
-      best_genetic = all_genetic[i]
+    if(best_genetic_target_value > targetValue ):
+      best_genetic = deepcopy(all_genetic[i])#[:]
       best_genetic_target_value = targetFunction(best_genetic)
-  print('Best Genetic Target Value: ', best_genetic_target_value)
+
+  print('\n','----- Best Genetic Target Value: ', best_genetic_target_value, '-----')
+  targetFunction(best_genetic, True)
+  print('-------------------------------------')
   return
 
 def main():
@@ -168,7 +208,7 @@ def main():
     selectNextGeneration(i+1) # 選擇: 選擇下一代基因
     crossover() # 交配
     muation() # 突變 => 突變一點太小了，這邊直接突變一個工人的全部
-    muation() # 
+    muation()
 
 if __name__ == "__main__":
     main()
